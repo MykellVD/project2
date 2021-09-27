af1a90a62fa44d6842ce9b592ad044f74cc6e8f6
justf <mvdfam@gmail.com>
2021-09-27 14:47:35 -0400

    Initial commit

---
+++ src/project2/GUI1024Panel.java
@@ -0,0 +1,116 @@
+package project2;
+
+import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+
+public class GUI1024Panel extends JPanel {
+
+    private JLabel[][] gameBoardUI;
+    private NumberGameArrayList gameLogic;
+
+    public GUI1024Panel() {
+        gameLogic = new NumberGameArrayList();
+        gameLogic.resizeBoard(4, 4, 16);
+
+        setBorder(BorderFactory.createLineBorder(Color.ORANGE));
+        setLayout(new GridLayout(4, 4));
+
+        gameBoardUI = new JLabel[4][4];
+
+        Font myTextFont = new Font(Font.SERIF, Font.BOLD, 40);
+        for (int k = 0; k < gameBoardUI.length; k++)
+            for (int m = 0; m < gameBoardUI[k].length; m++) {
+                gameBoardUI[k][m] = new JLabel();
+                gameBoardUI[k][m].setFont(myTextFont);
+                gameBoardUI[k][m].setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
+                gameBoardUI[k][m].setPreferredSize(new Dimension(100, 100));
+                add(gameBoardUI[k][m]);
+            }
+
+        gameLogic.reset();
+        updateBoard();
+        setFocusable(true);
+        addKeyListener(new SlideListener());
+    }
+
+    private void updateBoard() {
+        for (JLabel[] row : gameBoardUI)
+            for (JLabel s : row) {
+                s.setText("");
+            }
+
+        ArrayList<Cell> out = gameLogic.getNonEmptyTiles();
+        if (out == null) {
+            JOptionPane.showMessageDialog(null, "Incomplete implementation getNonEmptyTiles()");
+            return;
+        }
+        for (Cell c : out) {
+            JLabel z = gameBoardUI[c.row][c.column];
+            z.setText(String.valueOf(Math.abs(c.value)));
+            z.setForeground(c.value > 0 ? Color.BLACK : Color.RED);
+        }
+    }
+
+    private class SlideListener implements KeyListener, ActionListener {
+        @Override
+        public void keyTyped(KeyEvent e) { }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+            boolean moved = false;
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_UP:
+                    moved = gameLogic.slide(SlideDirection.UP);
+                    break;
+                case KeyEvent.VK_LEFT:
+                    moved = gameLogic.slide(SlideDirection.LEFT);
+                    break;
+                case KeyEvent.VK_DOWN:
+                    moved = gameLogic.slide(SlideDirection.DOWN);
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    moved = gameLogic.slide(SlideDirection.RIGHT);
+                    break;
+                case KeyEvent.VK_U:
+                    try {
+                        System.out.println("Attempt to undo");
+                        gameLogic.undo();
+                        moved = true;
+                    } catch (IllegalStateException exp) {
+                        JOptionPane.showMessageDialog(null, "Can't undo beyond the first move");
+                        moved = false;
+                    }
+            }
+            if (moved) {
+                updateBoard();
+                if (gameLogic.getStatus().equals(GameStatus.USER_WON))
+                    JOptionPane.showMessageDialog(null, "You won");
+                else if (gameLogic.getStatus().equals(GameStatus.USER_LOST)) {
+                    int resp = JOptionPane.showConfirmDialog(null, "Do you want to play again?", "TentOnly Over!",
+                            JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+                    if (resp == JOptionPane.YES_OPTION) {
+                        gameLogic.reset();
+                        updateBoard();
+                    } else {
+                        System.exit(0);
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) { }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+}
