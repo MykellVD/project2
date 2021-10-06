299f2d7549f9f4761e2601f806bb8825b116cdb9
UmlorJake <59674943+UmlorJake@users.noreply.github.com>
2021-10-06 09:44:39 -0400

    Add files via upload

--- src/project2/GUI1024.java
+++ src/project2/GUI1024.java
@@ -1,11 +1,35 @@
-package project2;
+package CISProjects.Project2;
 
 import javax.swing.*;
+import java.awt.event.KeyEvent;
 
 public class GUI1024 {
     public static void main(String arg[]){
+        JMenuBar menuBar;
+        JMenu menu;
+        JMenu undoButton;
+        JMenuItem highScore;
+        JMenuItem newGame;
+        JMenuItem resizeBoard;
+
+
         JFrame gui = new JFrame ("Welcome to 1024!");
         gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        menuBar = new JMenuBar();
+        gui.setJMenuBar(menuBar);
+
+        menu = new JMenu("Menu");
+        undoButton = new JMenu("Undo");
+        highScore = new JMenuItem("High Score");
+        newGame = new JMenuItem("New Game");
+        resizeBoard = new JMenuItem("Resize Board");
+
+        menuBar.add(menu);
+        menuBar.add(undoButton);
+        menu.add(highScore);
+        menu.add(newGame);
+        menu.add(resizeBoard);
 
 
 
@@ -13,7 +37,7 @@ public class GUI1024 {
         //panel.setFocusable(true);
         gui.getContentPane().add(panel);
 
-        gui.setSize(500,500);
+        gui.setSize(500, 500);
         gui.setVisible(true);
     }
 }
